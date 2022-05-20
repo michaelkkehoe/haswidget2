bc64aeb03ed8ef534dc3ef5c332e527afbcd758d
michaelkkehoe <4047274+michaelkkehoe@users.noreply.github.com>
2022-05-19 18:42:13 -0700

    Update __init__.py

--- custom_components/swidget/__init__.py
+++ custom_components/swidget/__init__.py
@@ -1,22 +1,27 @@
 """The Swidget integration."""
 from __future__ import annotations
 
+from pyswidget import SwidgetDevice, SwidgetException
+
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
 
-# TODO List the platforms that you want to support.
-# For your initial PR, limit it to 1 platform.
-PLATFORMS: list[Platform] = [Platform.LIGHT]
+from .const import DOMAIN, PLATFORMS
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Swidget from a config entry."""
     # TODO Store an API object for your platforms to access
     # hass.data[DOMAIN][entry.entry_id] = MyApi(...)
-
+    try:
+        device: SwidgetDevice = await SwidgetDevice().ping()
+    except as ex:
+        raise ConfigEntryNotReady from ex
+        
+    hass.data[DOMAIN][entry.entry_id]
     hass.config_entries.async_setup_platforms(entry, PLATFORMS)
 
     return True

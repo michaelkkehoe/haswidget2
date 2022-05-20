94289594672e19b69ce86a0d1c642890864240f4
michaelkkehoe <4047274+michaelkkehoe@users.noreply.github.com>
2022-05-19 18:46:38 -0700

    Update __init__.py

--- custom_components/swidget/__init__.py
+++ custom_components/swidget/__init__.py
@@ -1,6 +1,8 @@
 """The Swidget integration."""
 from __future__ import annotations
 
+import logging
+
 from pyswidget import SwidgetDevice, SwidgetException
 
 from homeassistant.config_entries import ConfigEntry
@@ -11,17 +13,19 @@ from .const import DOMAIN
 
 from .const import DOMAIN, PLATFORMS
 
+_LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Swidget from a config entry."""
     # TODO Store an API object for your platforms to access
     # hass.data[DOMAIN][entry.entry_id] = MyApi(...)
+    log.info(entry.data)
     try:
         device: SwidgetDevice = await SwidgetDevice().ping()
     except as ex:
         raise ConfigEntryNotReady from ex
         
-    hass.data[DOMAIN][entry.entry_id]
+    hass.data[DOMAIN][entry.entry_id] = SwidgetDataUpdateCoordinator(hass, device)
     hass.config_entries.async_setup_platforms(entry, PLATFORMS)
 
     return True
@@ -31,5 +35,4 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
         hass.data[DOMAIN].pop(entry.entry_id)
-
     return unload_ok

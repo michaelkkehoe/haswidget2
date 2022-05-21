62676da7418995844b3b82586d068fd1825664cb
michaelkkehoe <4047274+michaelkkehoe@users.noreply.github.com>
2022-05-20 22:14:27 -0700

    Update coordinator.py

--- custom_components/swidget/coordinator.py
+++ custom_components/swidget/coordinator.py
@@ -49,8 +49,6 @@ class SwidgetDataUpdateCoordinator(DataUpdateCoordinator):
     async def _async_update_data(self) -> None:
         """Fetch all device and sensor data from api."""
         try:
-            await self.device.update(update_children=self.update_children)
+            await self.device.update()
         except SwidgetException as ex:
             raise UpdateFailed(ex) from ex
-        finally:
-            self.update_children = True

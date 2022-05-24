5b07f3f9dd96c132fdbfcaed47bb1f98cc93b064
michaelkkehoe <4047274+michaelkkehoe@users.noreply.github.com>
2022-05-23 17:52:10 -0700

    Create swidgetdimmer.py

---
+++ custom_components/swidget/pyswidget/swidgetdimmer.py
@@ -0,0 +1,17 @@
+from pyswidget.device import (
+    DeviceType,
+    SwidgetDevice
+)
+
+
+class SwidgetDimmer(SwidgetDevice):
+
+    def __init__(self, host,  secret_key: str, ssl: bool) -> None:
+        super().__init__(host=host, secret_key=secret_key, ssl=ssl)
+        self._device_type = DeviceType.Dimmer
+
+    async def set_brightness(self, brightness):
+        """Set the brightness of the device."""
+        await self.send_command(
+            assembly="host", component="0", function="level", command={"now": brightness}
+        )

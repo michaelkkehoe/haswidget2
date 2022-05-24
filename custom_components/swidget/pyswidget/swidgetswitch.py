d5f092527e1f881c2a09b2f5f3e5bc2d3c8b3dd6
michaelkkehoe <4047274+michaelkkehoe@users.noreply.github.com>
2022-05-23 17:52:39 -0700

    Create swidgetswitch.py

---
+++ custom_components/swidget/pyswidget/swidgetswitch.py
@@ -0,0 +1,10 @@
+from pyswidget.device import (
+    DeviceType,
+    SwidgetDevice
+)
+
+class SwidgetSwitch(SwidgetDevice):
+
+    def __init__(self, host,  secret_key: str, ssl: bool) -> None:
+        super().__init__(host=host, secret_key=secret_key, ssl=ssl)
+        self._device_type = DeviceType.Switch

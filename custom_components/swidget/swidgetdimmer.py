162249f56f42e17985f202d72849e9224a0f2567
michaelkkehoe <4047274+michaelkkehoe@users.noreply.github.com>
2022-05-24 15:16:35 -0700

    Update swidgetdimmer.py

--- custom_components/swidget/swidgetdimmer.py
+++ custom_components/swidget/swidgetdimmer.py
@@ -2,6 +2,7 @@ from .device import (
     DeviceType,
     SwidgetDevice
 )
+from 
 
 
 class SwidgetDimmer(SwidgetDevice):
@@ -9,6 +10,18 @@ class SwidgetDimmer(SwidgetDevice):
     def __init__(self, host,  secret_key: str, ssl: bool) -> None:
         super().__init__(host=host, secret_key=secret_key, ssl=ssl)
         self._device_type = DeviceType.Dimmer
+
+    @property  # type: ignore
+    def brightness(self) -> int:
+        """Return current brightness on dimmers.
+
+        Will return a range between 0 - 100.
+        """
+        if not self.is_dimmable:
+            raise SwidgetException("Device is not dimmable.")
+
+        sys_info = self.sys_info
+        return int(sys_info["brightness"])
 
     async def set_brightness(self, brightness):
         """Set the brightness of the device."""

a476c95eb3739a5519a3ee93c41d9f1d1ab7abd1
michaelkkehoe <4047274+michaelkkehoe@users.noreply.github.com>
2022-05-23 20:56:39 -0700

    Update config_flow.py

--- custom_components/swidget/config_flow.py
+++ custom_components/swidget/config_flow.py
@@ -4,7 +4,8 @@ from __future__ import annotations
 import logging
 from typing import Any
 
-from myswidget.exceptions import SwidgetException
+from device import SwidgetDevice
+from exceptions import SwidgetException
 import voluptuous as vol
 
 from homeassistant import config_entries

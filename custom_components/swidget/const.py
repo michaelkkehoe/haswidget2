cfbfec9cb7ff2b3bba835c2a175a6c385e240ee7
michaelkkehoe <4047274+michaelkkehoe@users.noreply.github.com>
2022-05-25 15:36:27 -0700

    Update const.py

--- custom_components/swidget/const.py
+++ custom_components/swidget/const.py
@@ -5,5 +5,5 @@ from typing import Final
 from homeassistant.const import Platform
 
 DOMAIN = "swidget"
-PLATFORMS: Final = [Platform.LIGHT]
+PLATFORMS: Final = [Platform.BUTTON, Platform.LIGHT]
 # PLATFORMS: Final = [Platform.LIGHT, Platform.SENSOR, Platform.SWITCH]

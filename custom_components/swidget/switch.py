5f6e286394b7024328e4880c23ac7e96104ecf8c
michaelkkehoe <4047274+michaelkkehoe@users.noreply.github.com>
2022-05-26 21:41:18 -0700

    Create switch.py

---
+++ custom_components/swidget/switch.py
@@ -0,0 +1,58 @@
+"""Support for TPLink lights."""
+from __future__ import annotations
+
+from collections.abc import Sequence
+import logging
+from typing import Any, Final, cast
+
+from .swidgetoutlet import SwidgetOutlet
+import voluptuous as vol
+
+from homeassistant.components.light import (
+    SwitchEntity,
+)
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers import entity_platform
+import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
+from .const import DOMAIN
+from .coordinator import SwidgetDataUpdateCoordinator
+from .entity import CoordinatedSwidgetEntity, async_refresh_after
+
+_LOGGER = logging.getLogger(__name__)
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    config_entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
+    coordinator: SwidgetDataUpdateCoordinator = hass.data[DOMAIN][config_entry.entry_id]
+    if coordinator.device.is_outlet:
+        async_add_entities(
+            [SwidgetPlugSwitch(cast(SwidgetOutlet, coordinator.device), coordinator)]
+        )
+
+
+
+class SwidgetPlugSwitch(CoordinatedSwidgetEntity, SwitchEntity):
+    """Representation of a TPLink Smart Plug switch."""
+
+    def __init__(
+        self,
+        device: SmartDevice,
+        coordinator: SwidgetDataUpdateCoordinator,
+    ) -> None:
+        """Initialize the switch."""
+        super().__init__(device, coordinator)
+
+    @async_refresh_after
+    async def async_turn_on(self, **kwargs: Any) -> None:
+        """Turn the switch on."""
+        await self.device.turn_on()
+
+    @async_refresh_after
+    async def async_turn_off(self, **kwargs: Any) -> None:
+        """Turn the switch off."""
+        await self.device.turn_off()

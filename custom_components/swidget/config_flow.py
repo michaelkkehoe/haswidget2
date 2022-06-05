b8377e2086edc0d41e588289b0c47ae46d6fa180
Michael Kehoe <michael.k.kehoe@gmail.com>
2022-06-04 17:58:13 -0700

    fixes

--- custom_components/swidget/config_flow.py
+++ custom_components/swidget/config_flow.py
@@ -10,6 +10,7 @@ import voluptuous as vol
 
 
 from homeassistant import config_entries
+from homeassistant.const import CONF_DEVICE, CONF_HOST, CONF_MAC
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResult
 from homeassistant.helpers import device_registry as dr

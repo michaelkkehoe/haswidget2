a34982dd75e7c9a29ce641a667a589e36e92e5a1
Michael Kehoe <michael.k.kehoe@gmail.com>
2022-06-06 23:22:01 -0700

    Fix

--- custom_components/swidget/config_flow.py
+++ custom_components/swidget/config_flow.py
@@ -144,8 +144,6 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         _LOGGER.error(f"Configured Devices: {self._async_current_entries()}")
         self._discovered_devices = await async_discover_devices(self.hass)
         _LOGGER.error(f"Discovered Devices: {self._discovered_devices}")
-        for i in self._async_current_entries():
-            _LOGGER.error(f"Configured Device: {i.__dict__}")
         devices_name = {
             mac: f"{mac}"
             for mac, device in self._discovered_devices.items()
